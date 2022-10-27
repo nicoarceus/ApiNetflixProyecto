b60ab27b423b5228243ca2afa8a993a8fa18d870
Nicolas Zamorano <na.zamovasquez@gmail.com>
2022-10-26 22:00:02 -0300

    api fase final

--- Api/Api/Controllers/PeliculaController.cs
+++ Api/Api/Controllers/PeliculaController.cs
@@ -13,19 +13,19 @@ namespace Api.Controllers
         public PeliculaController()
         {
             this.Lista.Add(new Pelicula() { Id = 1, Genero = "Anime", Nombre = "Naruto",  FechaPublicacion = "0291029", Director = "el señor de las sopaipillas",Duracion = 150 });
-            this.Lista.Add(new Pelicula() { Id = 2, Genero = "Anime", Nombre = "Kung fu Panda", FechaPublicacion = "0291029", Director = "Jack Black", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 3, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 4, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 5, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 6, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 7, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 8, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 9, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 10, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 11, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 12, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 13, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
-            this.Lista.Add(new Pelicula() { Id = 14, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Duracion = 500 });
+            this.Lista.Add(new Pelicula() { Id = 2, Genero = "Animada/infantil", Nombre = "Kung fu Panda", FechaPublicacion = "0291029", Director = "Jack Black", Duracion = 90 });
+            this.Lista.Add(new Pelicula() { Id = 3, Genero = "Deportes/drama", Nombre = "Juego de honor", FechaPublicacion = "12/07/2005", Director = "Thomas Carter", Duracion = 136 });
+            this.Lista.Add(new Pelicula() { Id = 4, Genero = "Comedia", Nombre = "Ted 2", FechaPublicacion = "12/07/2015", Director = "Seth MacFarlane", Duracion = 115 });
+            this.Lista.Add(new Pelicula() { Id = 5, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "Sasuke kun", Duracion = 95 });
+            this.Lista.Add(new Pelicula() { Id = 6, Genero = "Anime", Nombre = "Naruto", FechaPublicacion = "0291029", Director = "Sasuke kun", Duracion = 91 });
+            this.Lista.Add(new Pelicula() { Id = 7, Genero = "Terror", Nombre = "Alien 2", FechaPublicacion = "12/04/2003", Director = "James Cameron", Duracion = 95 });
+            this.Lista.Add(new Pelicula() { Id = 8, Genero = "Terror", Nombre = "Alien 2", FechaPublicacion = "12/04/2003", Director = "James Cameron", Duracion = 95 });
+            this.Lista.Add(new Pelicula() { Id = 9, Genero = "Terror", Nombre = "Alien 2", FechaPublicacion = "12/04/2003", Director = "James Cameron", Duracion = 95 });
+            this.Lista.Add(new Pelicula() { Id = 10, Genero = "Anime", Nombre = "Mi vecino totoro", FechaPublicacion = "2004", Director = "Studio Gibli", Duracion = 90 });
+            this.Lista.Add(new Pelicula() { Id = 11, Genero = "Anime", Nombre = "Mi vecino totoro", FechaPublicacion = "2004", Director = "Studio Gibli", Duracion = 90 });
+            this.Lista.Add(new Pelicula() { Id = 12, Genero = "Anime", Nombre = "La princesa Mononoke", FechaPublicacion = "2004", Director = "Studio Gibli", Duracion = 97 });
+            this.Lista.Add(new Pelicula() { Id = 13, Genero = "Anime", Nombre = "La princesa Mononoke", FechaPublicacion = "2004", Director = "Studio Gibli", Duracion = 97 });
+            this.Lista.Add(new Pelicula() { Id = 14, Genero = "Anime", Nombre = "La princesa Mononoke", FechaPublicacion = "2004", Director = "Studio Gibli", Duracion = 97 });
 
         }
 
--- Api/Api/Controllers/SerieController.cs
+++ Api/Api/Controllers/SerieController.cs
@@ -13,27 +13,26 @@ namespace Api.Controllers
         public SerieController()
         {
             this.Lista.Add(new Serie() { Id = 1, Genero = "Anime", Nombre = "Naruto", Temporada = 3 ,FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500});
-            this.Lista.Add(new Serie() { Id = 2, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 3, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 4, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 5, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 6, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 7, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 8, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 9, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 10, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 11, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 12, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 13, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-
-            this.Lista.Add(new Serie() { Id = 14, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 15, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 16, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 17, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 18, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 19, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 20, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
-            this.Lista.Add(new Serie() { Id = 21, Genero = "Anime", Nombre = "Naruto", Temporada = 3, FechaPublicacion = "0291029", Director = "el señor de las sopaipillas", Capitulos = 500 });
+            this.Lista.Add(new Serie() { Id = 2, Genero = "Reality", Nombre = "El topo", Temporada = 1, FechaPublicacion = "10/10/2022", Director = "el señor de las sopaipillas", Capitulos = 10 });
+            this.Lista.Add(new Serie() { Id = 3, Genero = "Reality", Nombre = "El topo", Temporada = 1, FechaPublicacion = "10/10/2022", Director = "el señor de las sopaipillas", Capitulos = 10 });
+            this.Lista.Add(new Serie() { Id = 4, Genero = "Infatil", Nombre = "Gravity Falls", Temporada = 3, FechaPublicacion = "10/10/2015", Director = "La araña de rincón", Capitulos = 36 });
+            this.Lista.Add(new Serie() { Id = 5, Genero = "Infatil", Nombre = "Gravity Falls", Temporada = 3, FechaPublicacion = "10/10/2015", Director = "La araña de rincón", Capitulos = 36 });
+            this.Lista.Add(new Serie() { Id = 6, Genero = "Infatil", Nombre = "Gravity Falls", Temporada = 3, FechaPublicacion = "10/10/2015", Director = "La araña de rincón", Capitulos = 36 });
+            this.Lista.Add(new Serie() { Id = 7, Genero = "Drama", Nombre = "The Walking Dead", Temporada = 10, FechaPublicacion = "10/10/2012", Director = "George Romero", Capitulos = 120 });
+            this.Lista.Add(new Serie() { Id = 8, Genero = "Drama", Nombre = "The Walking Dead", Temporada = 10, FechaPublicacion = "10/10/2012", Director = "George Romero", Capitulos = 120 });
+            this.Lista.Add(new Serie() { Id = 9, Genero = "Drama", Nombre = "The Walking Dead", Temporada = 10, FechaPublicacion = "10/10/2012", Director = "George Romero", Capitulos = 120 });
+            this.Lista.Add(new Serie() { Id = 10, Genero = "K-Drama", Nombre = "Tribunal de menores", Temporada = 1, FechaPublicacion = "10/06/2022", Director = "Algun koreano famoso", Capitulos = 10 });
+            this.Lista.Add(new Serie() { Id = 11, Genero = "K-Drama", Nombre = "Tribunal de menores", Temporada = 1, FechaPublicacion = "10/06/2022", Director = "Algun koreano famoso", Capitulos = 10 });
+            this.Lista.Add(new Serie() { Id = 12, Genero = "K-Drama", Nombre = "Tribunal de menores", Temporada = 1, FechaPublicacion = "10/06/2022", Director = "Algun koreano famoso", Capitulos = 10 });
+            this.Lista.Add(new Serie() { Id = 13, Genero = "K-Drama", Nombre = "Facultad de derecho", Temporada = 1, FechaPublicacion = "10/06/2021", Director = "Algun koreano nashee", Capitulos = 16 });
+            this.Lista.Add(new Serie() { Id = 14, Genero = "K-Drama", Nombre = "Facultad de derecho", Temporada = 1, FechaPublicacion = "10/06/2021", Director = "Algun koreano nashee", Capitulos = 16 });
+            this.Lista.Add(new Serie() { Id = 15, Genero = "K-Drama", Nombre = "Facultad de derecho", Temporada = 1, FechaPublicacion = "10/06/2021", Director = "Algun koreano nashee", Capitulos = 16 });
+            this.Lista.Add(new Serie() { Id = 16, Genero = "Anime", Nombre = "Pokemon", Temporada = 16, FechaPublicacion = "13/07/2000", Director = "los señores de las pizzas", Capitulos = 1500 });
+            this.Lista.Add(new Serie() { Id = 17, Genero = "Anime", Nombre = "Pokemon", Temporada = 16, FechaPublicacion = "13/07/2000", Director = "los señores de las pizzas", Capitulos = 1500 });
+            this.Lista.Add(new Serie() { Id = 18, Genero = "Anime", Nombre = "Pokemon", Temporada = 16, FechaPublicacion = "13/07/2000", Director = "los señores de las pizzas", Capitulos = 1500 });
+            this.Lista.Add(new Serie() { Id = 19, Genero = "Anime", Nombre = "One piece", Temporada = 20, FechaPublicacion = "13/07/1999", Director = "Miyazaki", Capitulos = 1030 });
+            this.Lista.Add(new Serie() { Id = 20, Genero = "Anime", Nombre = "One piece", Temporada = 20, FechaPublicacion = "13/07/1999", Director = "Miyazaki", Capitulos = 1030 });
+            this.Lista.Add(new Serie() { Id = 21, Genero = "Anime", Nombre = "One piece", Temporada = 20, FechaPublicacion = "13/07/1999", Director = "Miyazaki", Capitulos = 1030 });
         }
 
         [HttpGet]
@@ -42,5 +41,8 @@ namespace Api.Controllers
             return Lista;
 
         }
+        
+
+      
     }
 }
